f26425bd0739db9abd7acfb4a4825cb02ebe9528
Nadia Mehmood <nadiamehmood66@gmail.com>
2026-03-25 20:58:51 +0500

    modifying

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -3,17 +3,17 @@ const { test, expect } = require('@playwright/test');
 test('Test 1 - Google', async ({ page }) => {
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
-  console.log('Test 1')
+  console.log('Test 11')
 });
 
 test('Test 2 - Playwright', async ({ page }) => {
   await page.goto('https://playwright.dev');
   await expect(page).toHaveTitle(/Playwright/);
-  console.log('Test 2')
+  console.log('Test 22')
 });
 
 test('Test 3 - GitHub', async ({ page }) => {
   await page.goto('https://github.com');
   await expect(page).toHaveTitle(/GitHub/);
-  console.log('Test 3')
+  console.log('Test 33')
 });
