5f5cfb39682a6778f12b9a6f74b588d6135cc04c
Nadia Mehmood <nadiamehmood66@gmail.com>
2026-03-25 21:32:59 +0500

    feature1 branch commit

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -3,17 +3,18 @@ const { test, expect } = require('@playwright/test');
 test('Test 1 - Google', async ({ page }) => {
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
-  console.log('Test 11')
+  console.log('Test 1')
 });
 
 test('Test 2 - Playwright', async ({ page }) => {
   await page.goto('https://playwright.dev');
   await expect(page).toHaveTitle(/Playwright/);
-  console.log('Test 22')
+  console.log('Test 2')
 });
 
 test('Test 3 - GitHub', async ({ page }) => {
   await page.goto('https://github.com');
   await expect(page).toHaveTitle(/GitHub/);
-  console.log('Test 33')
+  console.log('Test 3')
+  console.log('Test 4')
 });
